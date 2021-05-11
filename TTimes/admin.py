1f783726aaf33786c70c11b1278ceb1192bd317e
branch <j.branch.t@gmail,com>
2021-05-11 16:51:35 +0900

    update admin.py

--- TTimes/admin.py
+++ TTimes/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext, gettext_lazy as _
 # Register your models here.
 
 admin.site.register(ParentCompanyModel)
-admin.site.register(ChildCompanyModel)
 admin.site.register(StaffModel)
 admin.site.register(AttendanceModel)
+
+@admin.register(ChildCompanyModel)
+class AdminChildCompanyModel(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('name', 'password')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ('name', 'email', 'is_staff')
+    search_fields = ('name', 'email')
+    filter_horizontal = ()
--- TTimes/urls.py
+++ TTimes/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('signup/', signupview, name='signup'),
     path('login/', loginview, name='login'),
     path('attendance/', attendanceview, name='attendance'),
     path('staffpayment/', staffpaymentview, name='staffpeyment'),
