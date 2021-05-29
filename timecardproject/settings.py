c41b03f289bc3e8db0f7cac06fdb0147771346f0
mojamoja16 <hideomoja16@gmail.com>
2021-05-29 15:33:18 +0900

    css moja

--- timecardproject/settings.py
+++ timecardproject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'TTimes.apps.TtimesConfig',
-    'bootstrap4',
+    #'bootstrap4',
     'widget_tweaks',
 ]
 
@@ -121,10 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfile")
 STATICFILES_DIRS = (
     [
-        os.path.join(BASE_DIR, "TTimes/static"),
+        os.path.join(BASE_DIR, "static"),
     ]
 )
 
